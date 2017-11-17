8dded435bb436dc989fa23f9f99049aa6d3e49c1
myseital <myseital@163.com>
2017-11-17 16:55:14 +0800

    test

--- src/main/java/com/mao/common/utils/Test.java
+++ src/main/java/com/mao/common/utils/Test.java
@@ -5,6 +5,6 @@ package com.mao.common.utils;
  */
 public class Test {
     public static void main(String[] args) {
-        System.err.println("hhh");
+        System.err.println("hhhzzzz");
     }
 }
