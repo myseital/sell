6a4703f1b1c8f3bf59ef503d73e802faa09e4d4b
myseital <myseital@163.com>
2017-11-17 16:51:07 +0800

    init d

---
+++ src/main/java/com/mao/common/utils/Test.java
@@ -0,0 +1,10 @@
+package com.mao.common.utils;
+
+/**
+ * Created by win10 on 2017/11/17.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.err.println("hhh");
+    }
+}
