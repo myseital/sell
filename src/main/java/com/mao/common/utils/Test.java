e2304a623b2363805836ff5726f580e8e7400412
myseital <myseital@163.com>
2017-11-20 09:06:39 +0800

    local

--- src/main/java/com/mao/common/utils/Test.java
+++ src/main/java/com/mao/common/utils/Test.java
@@ -1,7 +1,7 @@
 package com.mao.common.utils;
 
 /**
- * Created by win10 on 2017/11/17.
+ * Created by myseital  on 2017/11/17.
  */
 public class Test {
     public static void main(String[] args) {
